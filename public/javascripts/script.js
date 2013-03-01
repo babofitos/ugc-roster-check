b0250350ea9237e9908ce81c4a41a998eacfaf40
babofitos <tehchoi@gmail.com>
2013-03-01 18:01:16 -0500

    initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,64 @@
+var submit = document.getElementById('check-btn') 
+
+submit.addEventListener('click', function(e) {
+  var request = new XMLHttpRequest()
+    , sids = document.getElementById('input-area').value
+    , matches = sids.match(/\d+:\d+:\d+/g)
+    , alert = document.getElementById('alert')
+
+  alert.classList.add('hidden')
+
+  if (!matches) {
+    alert.classList.remove('hidden')
+    alert.innerHTML = 'No valid STEAMID given'
+    submit.disabled = false
+    return
+  } else {
+    alert.innerHTML = 'Working...'
+  }
+
+  //disable button to prevent multiple submissions
+  submit.disabled = true
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 200) {
+        var data = JSON.parse(request.responseText)
+          , keys = Object.keys(data)
+          , tbody = document.getElementById('result-tbody')
+          , fragment = document.createDocumentFragment()
+          , tr
+          , td
+        
+        for (var i=0, len=keys.length;i<len;i++) {
+          tr = document.createElement('tr')
+          sidtd = document.createElement('td')
+          teamtd = document.createElement('td')
+          sidtd.appendChild(document.createTextNode(keys[i]))
+          if (!data[keys[i]]) {
+            teamtd.appendChild(document.createTextNode('None'))
+          } else {
+            teamtd.appendChild(document.createTextNode(data[keys[i]]))
+          }
+          tr.appendChild(sidtd)
+          tr.appendChild(teamtd)
+          fragment.appendChild(tr)
+        }
+        //show table
+        document.getElementById('results').classList.remove('hidden')
+        //reenable button
+        submit.disabled = false
+        //hide alert
+        alert.classList.add('hidden')
+        tbody.appendChild(fragment)
+      } else {
+        alert.innerHTML = 'Something bad happened'
+        alert.classList.remove('hidden')
+        submit.disabled = false
+      }
+    }
+    
+  }
+  request.open('post', '/')
+  request.setRequestHeader('Content-Type', 'application/json')
+  request.send(JSON.stringify(matches))
+}, false)
---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var cheerio = require('cheerio')
+  , request = require('request')
+  , $
+
+exports.index = function(req, res){
+  res.render('index');
+};
+
+exports.parse = function(req, res) {
+  getRosters(req.body, function(err, result) {
+    if (err) res.json(400)
+    else res.json(200, result)
+  })
+
+  function getRosters(input, cb) {
+    if (!input.test(/\d+:\d+:\d+/)) {
+      return cb(true)
+    }
+    request('http://ugcleague.com/rosters_tf2h.cfm', function(err, resp, body) {
+      var teams = {}
+
+      if (err) {
+        console.log(err)
+      } else {
+        $ = cheerio.load(body)
+        getTeam(function(result) {
+          cb(null, result)
+        })
+        
+        function getTeam(cb) {
+          var done = input.length
+            , teamsLength = 0
+
+          for (var i=0; i<done;i++) {
+            var id = input[i]
+              , len = $('.foottext').length-1
+              , foundAll = false
+
+            $('.foottext').each(function(index, el) {
+              var sid = $(this).text()
+              if (id === sid) {
+                var tr = $(this).parent().parent()
+                while (tr.attr('bgcolor') !== '000000') {
+                  tr = tr.prev()
+                }
+                teams[id] = tr.find('.link_blue').text()
+                teamsLength++
+              }
+              else if (index === len && !teams[id]) {
+                teams[id] = null
+                teamsLength++
+              }
+              if (teamsLength === done) {
+                cb(teams)
+                foundAll = true
+                return false //break out of .each
+              }
+            }) 
+            if (foundAll) { //break out of for loop once finding all teams
+              break
+            }
+          } 
+        }
+      }
+    })
+  }
+  
+}
